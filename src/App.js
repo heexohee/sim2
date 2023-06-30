39d4cc2a61f3596e4a2da723615ce5806a12af79
heexohee <rsoy2918@naver.com>
2023-06-30 20:20:46 +0900

    setNumber 함수

--- src/App.js
+++ src/App.js
@@ -12,8 +12,17 @@ function App() {
       Number State : {number}
     </div>
     <button onClick={()=>{
-      setNumber(number+1)
-    }}>누르면 up</button>
+// setNumber(number+1)
+setNumber((currentNumber)=>{
+  return currentNumber+1
+  // setNumber 함수를 써주는 것. 함수형으로 업데이트를 해줌.
+  // 직접 수정할 값이 아니라, 함수를 넣는 것. 
+  // 그 함수의 인자에는 현재 state를 가져올 수 있다.
+})
+    }}
+    >
+      
+      누르면 up</button>
   </>
   );
 }
